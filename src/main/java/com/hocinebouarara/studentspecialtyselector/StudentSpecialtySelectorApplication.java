b7576f443daabf4e9290031654b23da6f357b93c
hocinebouarara <hocinbouarara08@gmail.com>
2023-04-09 14:38:40 +0100

    init the project

---
+++ src/main/java/com/hocinebouarara/studentspecialtyselector/StudentSpecialtySelectorApplication.java
@@ -0,0 +1,13 @@
+package com.hocinebouarara.studentspecialtyselector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSpecialtySelectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentSpecialtySelectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hocinebouarara/studentspecialtyselector/StudentSpecialtySelectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hocinebouarara.studentspecialtyselector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSpecialtySelectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
